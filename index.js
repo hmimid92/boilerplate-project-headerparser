9ee0a75cbd263654154f3109b0b84b931e57ea80
HMIMID <ahmedhmimid177@gmail.com>
2024-11-03 13:51:19 +0100

    updating index.js file

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress: process.env.IP_ADDRESS, language: process.env.PREFFERED_LANGUAGE,
+software: process.env.SOFTWARE });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
